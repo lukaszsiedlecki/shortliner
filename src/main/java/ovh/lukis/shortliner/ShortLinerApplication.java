f13241f20914aad69fa8503ce65f0377110dc7bc
lukasz <siedlecki.lukasz92@gmail.com>
2025-01-26 07:58:38 +0100

    use postgres in cloud sql

--- src/main/java/ovh/lukis/shortliner/ShortLinerApplication.java
+++ src/main/java/ovh/lukis/shortliner/ShortLinerApplication.java
@@ -2,7 +2,6 @@ package ovh.lukis.shortliner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,11 +11,4 @@ public class ShortLinerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinerApplication.class, args);
     }
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
-
-
 }
