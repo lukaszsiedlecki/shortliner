59e26da058cd7a7ca12e06366ea6931e1d73ac79
Lukasz Siedlecki <siedlecki.lukasz92@gmail.com>
2026-06-24 14:02:10 +0200

    disable auth in dev profile via DevSecurityConfig

---
+++ src/main/java/ovh/lukis/shortliner/conf/DevSecurityConfig.java
@@ -0,0 +1,23 @@
+package ovh.lukis.shortliner.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("dev")
+@Configuration
+@EnableWebSecurity
+public class DevSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
